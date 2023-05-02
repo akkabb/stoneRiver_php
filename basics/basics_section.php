c6aeff237896da36c8d94f1ea46ed6f48cfdc99d
akkabb <akkabb@gmx.fr>
2023-05-02 10:22:07 +0200

    Try to make css only slide-menu

---
+++ basics/basics_section.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php require_once('include/head.php') ?>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="../index.php">Accueil</a>
+        </nav>
+    </header> 
+    <main></main>
+    <footer>
+        <p class="footer">2023 © Tous droits réservés</p>
+    </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -8,6 +8,25 @@
     <title>Learn Php Programming From Scratch</title>
 </head>
 <body>
+    <main>
+        <h1>PHP Course taking from Udemy</h1>
+        <p>Start with the basics :</p>
     
+        <aside class="header">
+            <!-- https://www.youtube.com/watch?v=IdMREOLsY4U help_me do the sidebar -->
+            <input type="checkbox" class="check">
+            <div class="toggle_cross">
+                <div class="line ln1"></div>
+                <div class="line ln2"></div>
+                <div class="line ln3"></div>
+            </div>
+            <ul class="menu">
+                <li><a href="basics/basics_section.php" class="link_basics">basics of php</a></li>
+            </ul>
+        </aside>
+    </main>
+    <footer>
+        <p class="footer">2023 © Tous droits réservés</p>
+    </footer>
 </body>
 </html>
