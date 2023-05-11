ba152ba653d2d273f7ddfe84abfdb9abe939d36b
akkabb <akkabb@gmx.fr>
2023-05-11 06:29:12 +0200

    Watched all the video from the section BB CODES

---
+++ BB Codes/bb_codes.php
@@ -0,0 +1,13 @@
+<?php
+$title = 'BB Codes';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php require_once('include/head.php');?>
+</head>
+<body>
+   <h1>I only watched the video. Now, know that exist</h1>
+    
+</body>
+</html>
