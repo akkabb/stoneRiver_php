de0cd7fcc0a82c42ba33340ddb52ee0c6192c961
akkabb <akkabb@gmx.fr>
2023-05-15 16:51:41 +0200

    Template Engine done!

--- Template_Engine/templateEngine.php
+++ Template_Engine/templateEngine.php
@@ -9,6 +9,16 @@ $title = "Template Engine";
     
 </head>
 <body>
-    
+    <?php
+    require_once('template.php');
+    $template= new Template;
+
+    $template->assign('username', 'Terry', );
+    $template->assign('age', 18);
+    $template->assign('fav_food', 'pizza');
+    $template->render('myTemplate');
+
+    // p rint_r($template);
+    ?>
 </body>
 </html>
