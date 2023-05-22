8957c457f76505174ef2258c67ad394904cbcfa3
akkabb <akkabb@gmx.fr>
2023-05-22 06:47:46 +0200

    FB app videos watched

---
+++ FB_App/fb_app.php
@@ -0,0 +1,12 @@
+<?php
+    $title = "Introduction to facebook application development";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php require_once('include/head.php');?>
+</head>
+<body>
+    <h1>Watched Videos only</h1>
+</body>
+</html>
--- Mini_shoppingCart/shopping_cart.php
+++ Mini_shoppingCart/shopping_cart.php
@@ -4,6 +4,7 @@
 ?>
 <head>
     <?php require_once('include/head.php');?>
+</head>
 <body>
     <main class="main_ShoppingCart">
     <?php 
